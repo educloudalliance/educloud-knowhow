741619f84c65c6075bb20340ebd09596c932c29c
Ville Jyrkkä <vjyrkka@gmail.com>
2014-03-19 20:01:00 +0200

    Add all files, except those in gitignore.

---
+++ qa-lang/fi/qa-lang-misc.php
@@ -0,0 +1,121 @@
+<?php
+/*
+	Swedish Translation for Question2Answer 1.6.1 http://www.question2answer.org/
+	
+	(c) 2013, Anders Hellman
+	Please submit any translation improvements or comments via:
+	http://www.vilkenapp.se/feedback	
+	or
+	ziah75 (at) gmail dot com
+	
+	File: sv/qa-lang-misc.php
+	Version: 1.1
+	Date: 2013-07-15
+	Description: Miscellaneous language phrases
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+	return array(
+		
+		'block_ip_button' => 'Blockera IP-adress',
+		'browse_categories' => 'Se kategorier',
+		'captcha_confirm_fix' => 'Undvik denna bekräftelse framöver, genom att ^5bekräfta din email adress^6.',
+		'captcha_error' => 'Vänligen fyll i anti-spam kontrollen',
+		'captcha_label' => 'Anti-spam kontroll:',
+		'captcha_login_fix' => 'Undvik denna bekräftelse framöver, genom att ^1logga in^2 eller ^3registrera dig^4.',
+		'feed_a_edited_prefix' => 'Svaret ändrat: ',
+		'feed_a_prefix' => 'Besvarad: ',
+		'feed_a_reshown_prefix' => 'Svaret visas igen: ',
+		'feed_a_selected_prefix' => 'Utvalt svar: ',
+		'feed_c_edited_prefix' => 'Kommentaren ändrad: ',
+		'feed_c_moved_prefix' => 'Kommentaren flyttad: ',
+		'feed_c_prefix' => 'Kommenterad: ',
+		'feed_c_reshown_prefix' => 'Kommentaren visas igen: ',
+		'feed_closed_prefix' => 'Stängd: ',
+		'feed_edited_prefix' => 'Ändrad: ',
+		'feed_hidden_prefix' => 'Dold: ',
+		'feed_not_found' => 'Flödet hittades inte',
+		'feed_recategorized_prefix' => 'Kategorin har ändrats: ',
+		'feed_reopened_prefix' => 'Öppna igen: ',
+		'feed_reshown_prefix' => 'Visas igen: ',
+		'feed_retagged_prefix' => 'Etiketter ändrade: ',
+		'feedback_email' => 'Din email: (valfritt)',
+		'feedback_empty' => 'Vänligen använd detta fält för kommentarer eller förslag',
+		'feedback_message' => 'Dina kommentarer eller förslag om ^:',
+		'feedback_name' => 'Ditt namn: (valfritt)',
+		'feedback_sent' => 'Ditt meddelande nedan har skickats - Tack.',
+		'feedback_title' => 'Skicka feedback',
+		'hide_all_ip_button' => 'Dölj alla inlägg från denna IP-adress',
+		'host_name' => 'Värdnamn (host name):',
+		'matches_blocked_ips' => 'Matchar blockerade IP-adresser:',
+		'message_empty' => 'Skriv meddelandet du vill skicka till denna användare',
+		'message_explanation' => 'Detta kommer skickas som en notifiering från ^. Din mailadress kommer inte avslöjas, såvida du inte själv anger den i meddelandet.',
+		'message_for_x' => 'Ditt meddelande till ^:',
+		'message_limit' => 'Du har sänt för många privata meddelanden - vänligen försök igen om en timme',
+		'message_must_login' => 'Vänligen ^1logga in^2 eller ^3registrera dig^4 för att skicka privata meddelanden.',
+		'message_recent_history' => 'Senaste meddelanden med ^',
+		'message_sent' => 'Ditt privata meddelande nedan har skickats',
+		'my_favorites_title' => 'Mina favoriter',
+		'nav_all_my_updates' => 'Alla mina uppdateringar',
+		'nav_my_content' => 'Mitt innehåll',
+		'nav_my_details' => 'Mina uppgifter',
+		'nav_my_favorites' => 'Mina favoriter',
+		'no_activity_from_x' => 'Ingen aktivitet från ^',
+		'no_favorite_categories' => 'Inga favoritkategorier',
+		'no_favorite_qs' => 'Inga favoritfrågor',
+		'no_favorite_tags' => 'Inga favoritetiketter',
+		'no_favorite_users' => 'Inga favoritanvändare',
+		'no_recent_updates' => 'Inga senaste nyheter',
+		'no_updates_content' => 'Inga senaste nyheter i mitt innehåll',
+		'no_updates_favorites' => 'Inga senaste nyheter i mina favoriter',
+		'private_message_title' => 'Skicka ett privat meddelande',
+		'recent_activity_from_x' => 'Senaste aktiviteten från ^',
+		'recent_updates_content' => 'Det senaste i mitt innehåll',
+		'recent_updates_favorites' => 'Det senaste i mina favoriter',
+		'recent_updates_title' => 'Det senaste för mig',
+		'site_in_maintenance' => 'Denna sajt är just nu avstängd för underhåll - vänligen återkom om en stund.',
+		'suggest_favorites_add' => 'Lägg till en fråga eller annat till dina favoriter, genom att klicka på ^ som du hittar nära sidans topp.',
+		'suggest_update_favorites' => 'För fler nyheter, lägg till saker till ^1dina favoriter^2.',
+		'unblock_ip_button' => 'Häv blockering av IP-adress',
+		//Additions from 1.6.1
+		'captcha_approve_fix' => "Denna kontroll kommer inte synas när ditt konto har godkänts.",
+		'form_security_again' => "Vänligen klicka igen för att bekräfta",
+		'form_security_reload' => "Vänligen ladda om sidan och försök igen",
+		'nav_user_activity' => "Senaste aktivitet",
+		'nav_user_as' => "Alla svar",
+		'nav_user_qs' => "Alla frågor",
+		'nav_user_wall' => "Vägg",
+		'your_a_commented' => "ditt svar kommenterat",
+		'your_a_edited' => "ditt svar ändrat",
+		'your_a_hidden' => "ditt svar dolt",
+		'your_a_questioned' => "fråga till ditt svar",
+		'your_a_reshown' => "ditt svar syns igen",
+		'your_a_selected' => "ditt svar utvalt",
+		'your_c_edited' => "din kommentar ändrad",
+		'your_c_followed' => "din kommentar har följts",
+		'your_c_hidden' => "din kommentar dold",
+		'your_c_moved' => "din kommentar flyttad",
+		'your_c_reshown' => "din kommentar syns igen",
+		'your_q_answered' => "din fråga är besvarad",
+		'your_q_closed' => "din fråga är stängd",
+		'your_q_commented' => "din fråga kommenterad",
+		'your_q_edited' => "din fråga ändrad",
+		'your_q_hidden' => "din fråga dold",
+		'your_q_recategorized' => "din fråga har ny kategori",
+		'your_q_reopened' => "din fråga åter öppen",
+		'your_q_reshown' => "din fråga syns igen",
+		'your_q_retagged' => "din fråga har nya etiketter",
+	);
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
---
+++ qa-lang/fi/qa-lang-users.php
@@ -0,0 +1,145 @@
+<?php
+/*
+	Swedish Translation for Question2Answer 1.6.1 http://www.question2answer.org/
+	
+	(c) 2013, Anders Hellman
+	Please submit any translation improvements or comments via:
+	http://www.vilkenapp.se/feedback	
+	or
+	ziah75 (at) gmail dot com
+	
+	File: sv/qa-lang-users.php
+	Version: 1.1
+	Date: 2013-07-15
+	Description: Language phrases for user management
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+	return array(
+		'about' => 'Om',
+		'avatar_default' => 'Standard',
+		'avatar_gravatar' => 'Visa min ^1Gravatar^2',
+		'avatar_label' => 'Avatar:',
+		'avatar_none' => 'Ingen',
+		'block_user_button' => 'Blockera användare',
+		'blocked_users' => 'Blockerade användare',
+		'change_password' => 'Byt lösenord',
+		'confirm_complete' => 'Tack - din email adress har nu bekräftats',
+		'confirm_emailed' => 'Ett mail med bekräftelselänk har skickats till dig. Vänligen klicka på länken i mailet för att bekräfta din email adress.',
+		'confirm_title' => 'Email adress bekräftelse',
+		'confirm_wrong_log_in' => 'Felaktig kod - vänligen ^1logga in^2 för att skicka en ny länk',
+		'confirm_wrong_resend' => 'Felaktig kod - vänligen logga in för att skicka en ny länk',
+		'edit_user_button' => 'Ändra denna användare',
+		'email_confirmed' => 'Bekräftad',
+		'email_exists' => 'Email adressen finns redan registrerad på ett konto',
+		'email_handle_label' => 'Email eller användarnamn:',
+		'email_invalid' => 'Email adressen är felaktigt angiven - vänligen kontrollera',
+		'email_label' => 'Email:',
+		'email_not_confirmed' => 'Inte bekräftad ännu',
+		'email_please_confirm' => 'Vänligen ^5bekräfta^6',
+		'email_required' => 'Email adress krävs - visas ej publikt',
+		'forgot_link' => 'Jag har glömt mitt lösenord',
+		'full_name' => 'Fullständigt namn',
+		'handle_empty' => 'Användarnamnet kan inte lämnas tomt',
+		'handle_exists' => 'Användarnamnet är upptaget - välj ett annat',
+		'handle_has_bad' => 'Användarnamnet får inte innehålla: ^',
+		'handle_label' => 'Användarnamn:',
+		'last_login_label' => 'Senaste inloggning:',
+		'last_write_label' => 'Senaste bidraget:',
+		'level_admin' => 'Administrator',
+		'level_editor' => 'Editor',
+		'level_expert' => 'Expert',
+		'level_moderator' => 'Moderator',
+		'level_super' => 'Super Administrator',
+		'location' => 'Plats',
+		'log_in_to_access' => 'Du kan nu ^1logga in^2 på ditt konto.',
+		'login_button' => 'Logga in',
+		'login_limit' => 'För många inloggningsförsök - vänligen prova igen om 1 timme',
+		'login_title' => 'Logga in',
+		'member_for' => 'Har varit medlem i:',
+		'member_type' => 'Typ:',
+		'new_password_1' => 'Nytt lösenord:',
+		'new_password_2' => 'Upprepa nytt lösenord:',
+		'no_blocked_users' => 'Det finns inga blockerade användare',
+		'no_permission' => 'Du har inte rättighet att göra detta',
+		'old_password' => 'Gamla lösenordet:',
+		'only_shown_admins' => '(visas endast för administratörer)',
+		'only_shown_moderators' => '(visas endast för moderatorer)',
+		'password_changed' => 'Lösenordet har ändrats',
+		'password_label' => 'Lösenord:',
+		'password_min' => 'Lösenordet måste minst ha ^ tecken',
+		'password_mismatch' => 'De nya lösenorden matchar inte',
+		'password_none' => 'Inget. För att logga in direkt, välj ett lösenord nedan.',
+		'password_sent' => 'Ditt nya lösenord har skickats till dig via email',
+		'password_wrong' => 'Felaktigt lösenord',
+		'profile_saved' => 'Profilen har sparats',
+		'register_button' => 'Registrera',
+		'register_title' => 'Registrera som ny användare',
+		'registered_user' => 'Registrerad användare',
+		'remember_label' => 'Kom ihåg mig på den här datorn',
+		'reset_code_another' => 'skicka igen',
+		'reset_code_emailed' => 'En återställningskod har mailats till dig',
+		'reset_code_label' => 'Kod:',
+		'reset_code_wrong' => 'Koden är felaktig',
+		'reset_title' => 'Återställ glömt lösenord',
+		'save_profile' => 'Spara profil',
+		'save_user' => 'Spara användare',
+		'send_confirm_button' => 'Skicka bekräftelselänk',
+		'send_password_button' => 'Skicka nytt lösenord',
+		'send_reset_button' => 'Skicka mail för att återställa lösenordet',
+		'send_reset_note' => 'Ett meddelande med instruktioner kommer skickas till din email adress.',
+		'special_users' => 'Specialanvändare',
+		'unblock_user_button' => 'Häv blockering av användare',
+		'user_blocked' => '(blockerad)',
+		'user_not_found' => 'Användaren kan inte hittas',
+		'website' => 'Webbplats',
+		'x_ago_from_y' => '^1 sedan från ^2',
+		//Added for 1.5
+		'add_user_x_favorites' => "Lägg till användaren ^ till mina favoriter",
+		'change_email_link' => " - ^1byt email^2",
+		'confirm_required' => "För att slutföra din registrering, vänligen klicka på bekräftelse-länken som har mailats till dig. Hittar du inte mailet, så kan du ^1begära ett nytt mail^2.",
+		'delete_user_button' => "Radera användare",
+		'hide_all_user_button' => "Dölj alla inlägg från denna användare",
+		'mass_mailings_explanation' => "Prenumerera på mail som skickas ut till alla användare",
+		'mass_mailings' => "Massutskick av mail:",
+		'password_to_set' => "Vänligen ställ in på din kontosida",
+		'private_messages_explanation' => "Tillåt användare att skicka mail till mig (utan att avslöja min email adress)",
+		'private_messages' => "Privata meddelanden:",
+		'register_limit' => "För många registreringar - försök igen om en timme",
+		'register_suspended' => "Registreringen av nya användare är tillfälligt avstängd. Vänligen försök igen senare.",
+		'remove_avatar' => "Radera avatar:",
+		'unsubscribe_complete' => "Du har avregistrerats från massutskick av mail som skickas ut från ^0. Du kan aktivera din prenumeration igen via din ^1kontosida^2.",
+		'unsubscribe_title' => "Avsluta prenumeration",
+		'unsubscribe_wrong_log_in' => "Felaktig kod - vänligen ^1logga in^2 för att avsluta din prenumeration",
+		'unsubscribe' => "Avsluta prenumeration:",
+		//Additions from 1.6.1
+		'approve_required' => "Vänligen vänta på att ditt konto godkänns eller ^1lägg till mer information^2.",
+		'approve_title' => "Användare väntar på godkännande",
+		'approve_user_button' => "Godkänn användare",
+		'approved_user' => "Godkänd användare",
+		'category_level_add' => " - ^1lägg till kategorispecifika rättigheter^2",
+		'category_level_in' => "för kategorin nedan:",
+		'category_level_label' => "Uppgraderad till:",
+		'category_level_none' => "Ingen uppgradering",
+		'level_for_category' => "^1 för ^2",
+		'level_in_general' => "allmänt",
+		'only_shown_editors' => "(visas endast för editors och högre rang)",
+		'only_shown_experts' => "(visas endast för experts och högre rang)",
+		'wall_posts' => "Vägg-inlägg:",
+		'wall_posts_explanation' => "Tillåt användare att skriva inlägg på din vägg (du kommer också få email)",
+		'registered_label' => "Registrerad:",
+		'remember' => "Kom ihåg",
+	);
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
