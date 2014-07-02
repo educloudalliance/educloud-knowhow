0382fc704d8148433590144961814ab62711c8eb
Ville Jyrkkä <vjyrkka@gmail.com>
2014-07-02 11:21:14 +0300

    Add finnish translations from markosu.

--- qa-lang/fi/qa-lang-profile.php
+++ qa-lang/fi/qa-lang-profile.php
@@ -1,62 +1,76 @@
 <?php
+	
 /*
-        Finnish Translation for Question2Answer 1.6.1 http://www.question2answer.org/
+	Question2Answer (c) Gideon Greenspan
 
-        (c) 2014, Jarkko Moilanen
-        Please submit any translation improvements or comments via:
-        http://www.github.com/kansallinenpilvivayla
-        or
-        jm60697 (at) gmail dot com
+	http://www.question2answer.org/
 
-        File: fi/qa-lang-profile.php
-        Version: 0.1
-        Date: 2014-02-21
-        Description: Language phrases commonly used throughout Q2A
+	
+	File: qa-include/qa-lang-profile.php
+	Version: See define()s at top of qa-include/qa-base.php
+	Description: Language phrases for user profile page
 
-        This program is free software; you can redistribute it and/or
-        modify it under the terms of the GNU General Public License
-        as published by the Free Software Foundation; either version 2
-        of the License, or (at your option) any later version.
 
-        This program is distributed in the hope that it will be useful,
-        but WITHOUT ANY WARRANTY; without even the implied warranty of
-        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-        GNU General Public License for more details.
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-        More about this license: http://www.question2answer.org/license.php
+	More about this license: http://www.question2answer.org/license.php
 */
+
 	return array(
 		'1_chosen_as_best' => ' (1 valittu parhaaksi)',
 		'1_down_vote' => '1 alaspäin peukutus',
 		'1_up_vote' => '1 peukutus ylös',
 		'1_with_best_chosen' => ' (1 valittu parhaaksi vastaukseksi)',
 		'activity_by_x' => '^:n aktiivisuus',
+		'answers_by_x' => '^:n vastaukset',		
 		'answers' => 'Vastaukset:',
 		'bonus_points' => 'Lisäpisteet:',
 		'comments' => 'Kommentit:',
+		'delete_wall_post_popup' => 'Poista tämä seinäviesti',
 		'extra_privileges' => 'Lisäoikeudet:',
 		'gave_out' => 'Antanut pois:',
 		'my_account_title' => 'Omat tiedot',
+		'no_answers_by_x' => '^ - ei vastauksia',
 		'no_posts_by_x' => '^ : Ei uusia avauksia',
+		'no_questions_by_x' => '^ - ei kysymyksiä',
 		'permit_anon_view_ips' => 'Näytä IP-osoitteet anonyymeille käyttäjille',
 		'permit_close_q' => 'Kaikkien kysymysten sulkeminen',
 		'permit_delete_hidden' => 'Piilotettujen kirjoitusten poistaminen',
 		'permit_edit_a' => 'Kaikkien vastausten muokkaaminen',
 		'permit_edit_c' => 'Kaikkien kommenttien muokkaaminen',
 		'permit_edit_q' => 'Kaikkien kysymysten muokkaaminen',
+		'permit_edit_silent' => 'Salli hiljainen muokkaus',
 		'permit_flag' => 'Roskapostiksi liputtaminen',
 		'permit_hide_show' => 'Viestien piilottaminen tai näyttäminen',
 		'permit_moderate' => 'Viestien hyväksyminen tai hylkääminen',
 		'permit_post_a' => 'Kysymyksiin vastaaminen',
 		'permit_post_c' => 'Kirjoittaa kommentteja',
 		'permit_post_q' => 'Esittää kysymyksiä',
+		'permit_post_wall' => 'Salli seinäkirjoitukset',
 		'permit_recat' => 'Vaihtaa kysymyksien kategoriaa',
 		'permit_retag' => 'Vaihtaa kysymysten tageja',
 		'permit_select_a' => 'Valita kysymyksiin paras vastaus',
 		'permit_view_q_page' => 'Lukea sisältöä',
+		'permit_view_voters_flaggers' => 'Näytä kuka äänesti tai liputti sisältöä',
 		'permit_vote_a' => 'Peukuttaa ylös sisältöä',
 		'permit_vote_down' => 'Peukuttaa alas sisältöä',
 		'permit_vote_q' => 'Peukuttaa kysymyksiä',
+		'post_wall_blocked' => 'Tämä käyttäjä ei salli seinäkirjoituksia',
+		'post_wall_button' => 'Kirjoita seinälle',
+		'post_wall_empty' => 'Lisää sisältöä jättääksesi viestin seinälle',
+		'post_wall_limit' => 'Liian monta seinäkirjoitusta - yritä uudelleen tunnin kuluttua',
+		'post_wall_must_be_approved' => 'Tunnuksesi pitää hyväksyä ennen kuin pystyt kirjoittamaan tälle seinälle.',
+		'post_wall_must_confirm' => 'Ole hyvä ja ^5vahvista sähköpostiosoitteesi^6 kirjoittaaksesi tälle seinälle.',
+		'post_wall_must_login' => 'Ole hyvä ja ^1kirjaudu^2 tai ^3rekisteröidy^4 kirjoittaaksesi tälle seinälle.',
+		'questions_by_x' => '^:n kysymykset',
 		'questions' => 'Sisällöt:',
 		'ranked_x' => ' (sijoitus #^)',
 		'received' => 'Saatu:',
@@ -67,29 +81,15 @@
 		'title' => 'Otsikko:',
 		'user_x' => 'Käyttäjä ^',
 		'voted_on' => 'äänesti kohdetta:',
+		'wall_for_x' => 'Seinän omistaa ^',
+		'wall_view_more' => 'Näytä lisää...',
 		'x_chosen_as_best' => ' (^ valittu parhaaksi)',
 		'x_down_votes' => '^ peukut alaspäin',
 		'x_up_votes' => '^ peukut ylöspäin',
 		'x_with_best_chosen' => ' (^ parasta vastausta)',
-		//Additions from 1.6.1
-		'answers_by_x' => "^:n vastaukset",
-		'delete_wall_post_popup' => "Poista tämä seinäviesti",
-		'no_answers_by_x' => "^ - ei vastauksia",
-		'no_questions_by_x' => "^ - ei kysymyksiä",
-		'permit_edit_silent' => "Salli hiljainen muokkaus",
-		'permit_post_wall' => "Salli seinäkirjoitukset",
-		'permit_view_voters_flaggers' => "Näytä kuka äänesti tai liputtu sisältöä",
-		'post_wall_blocked' => "Tämä käyttäjä ei salli seinäkirjoituksia",
-		'post_wall_button' => "Kirjoita seinälle",
-		'post_wall_empty' => "Lisää sisältöä jättääksesi viestin seinälle",
-		'post_wall_limit' => "Du får inte skriva fler vägg-inlägg den här timmen",
-		'post_wall_must_be_approved' => "Ditt konto måste godkännas för att du ska kunna skriva på den här väggen.",
-		'post_wall_must_confirm' => "Vänligen ^5bekräfta din mailadress^6 för att kunna skriva på den här väggen.",
-		'post_wall_must_login' => "^1Kirjaudu sisään^2 tai ^3rekisteröidy^4 kirjoittaaksesi seinälle.",
-		'questions_by_x' => "^:n kysymykset",
-		'wall_for_x' => "Seinän omistaa ^",
-		'wall_view_more' => "Näytä lisää...",
 	);
+	
+
 /*
 	Omit PHP closing tag to help avoid accidental output
 */
